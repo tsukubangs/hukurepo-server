f5d09b214c1ed0af174c1c8480619cbd1dff81e5
wmikefish <wmikefish@gmail.com>
2017-05-17 06:24:55 +0000

    change photo uploader config

--- app/uploaders/problem_photo_uploader.rb
+++ app/uploaders/problem_photo_uploader.rb
@@ -8,6 +8,8 @@ class ProblemPhotoUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  process convert: 'jpg'
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -36,14 +38,14 @@ class ProblemPhotoUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_whitelist
+     %w(jpg jpeg png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+     "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename
+   end
 
 end
