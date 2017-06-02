4c5dfb4b11ed743895bb080125626a3bcd391825
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2017-06-02 15:07:26 +0900

    fix routes of user

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
-    resource :users, only: [:create, :show]
-    resources :users, only: [:index]
+    resource :users, only: [:create]
+    resources :users, only: [:index, :show]
   end
 end
