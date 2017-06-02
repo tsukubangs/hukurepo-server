43e6609ce2a663dc11b27cdea1a230daa18c0fbd
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2017-06-02 11:58:16 +0900

    fix user create routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :users, only: []
   resources :problems
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   namespace :v1, defaults: { format: :json } do
-   resource :login, only: [:create], controller: :sessions
-   resource :users, only: [:create]
-   resources :users, only: [:index, :create]
- end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :v1, defaults: { format: :json } do
+    resource :login, only: [:create], controller: :sessions
+    resource :users, only: [:create, :show]
+    resources :users, only: [:index]
+  end
 end
