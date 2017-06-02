fc92ba7f62a4d053aa0080ba0787454efe8acd4d
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2017-06-02 15:51:00 +0900

    moved problem under V1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_for :users, only: []
-  resources :problems
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
     resource :users, only: [:create]
     resources :users, only: [:index, :show]
+    resources :problems
   end
 end
