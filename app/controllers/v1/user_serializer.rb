7394c9903fc64d8335b8214dd259dd6044b5e05a
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2017-06-02 15:05:32 +0900

    modified json render

--- app/controllers/v1/user_serializer.rb
+++ app/controllers/v1/user_serializer.rb
@@ -1,5 +1,6 @@
 module V1
   class UserSerializer < ActiveModel::Serializer
-    attributes :id, :email, :created_at, :updated_at
+    attributes :id, :email, :name, :gender, :age, :nationality, :image,
+               :created_at, :updated_at
   end
 end
