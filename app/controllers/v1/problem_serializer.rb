3ed5b1ea011513be41587628218ee2e703d5551a
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2017-06-02 17:36:49 +0900

    fix image url of json file on response

--- app/controllers/v1/problem_serializer.rb
+++ app/controllers/v1/problem_serializer.rb
@@ -1,6 +1,10 @@
 module V1
   class ProblemSerializer < ActiveModel::Serializer
-    attributes :id, :comment, :image, :latitude, :longitude, :user_id,
+    attributes :id, :comment, :image_url, :latitude, :longitude, :user_id,
                :created_at, :updated_at
+  end
+
+  def image_url
+    image.url
   end
 end
