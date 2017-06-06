16ef8f36a031aff52356e181b7908ba1bd879631
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2017-06-06 10:40:24 +0900

    modify image schema of problem serializer

--- app/controllers/v1/problem_serializer.rb
+++ app/controllers/v1/problem_serializer.rb
@@ -1,5 +1,5 @@
 module V1
-  class ProblemSerializer < ActiveModel::Serializer
+    class ProblemSerializer < ActiveModel::Serializer
     attributes :id, :comment, :image_url, :latitude, :longitude, :user_id,
                :created_at, :updated_at
   end
