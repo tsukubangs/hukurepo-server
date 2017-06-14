8cb5775d28184c0dc066786c671d1723a228e12d
wmikefish <wmikefish@gmail.com>
2017-06-14 15:36:05 +0900

    add get-problem-api nested users

--- app/controllers/v1/problems_controller.rb
+++ app/controllers/v1/problems_controller.rb
@@ -8,7 +8,14 @@ module V1
       render json: @problems, each_serializer: V1::ProblemSerializer
     end
 
+    # GET /v1/users/1/problems
+    def users
+      @problems = Problem.where(user_id: params[:user_id])
+      render json: @problems, each_serializer: V1::ProblemSerializer
+    end
+
     # GET /v1/problems/me
+    # GET /v1/users/me/problems
     def me
       @problems = Problem.where(user_id: current_user.id)
       render json: @problems, each_serializer: V1::ProblemSerializer
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   devise_for :users, only: []
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
     resource :users, only: [:create]
-    resources :users, only: [:index, :show]
+    resources :users, only: [:index, :show], shallow: true do
+      get 'me', on: :collection
+      get 'me/problems', to: 'problems#me', on: :collection
+      get 'problems', to: 'problems#users'
+    end
     resources :problems do
-      collection do
-        get 'me'
-      end
+      get 'me', on: :collection
     end
   end
 end
