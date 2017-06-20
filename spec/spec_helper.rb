f8bf6c58c03af22eca93d223f96539db0ba62e21
wmikefish <wmikefish@gmail.com>
2017-06-20 10:25:55 +0900

    configure rspec to apply custom helper module only api

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,10 +13,19 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'rails_helper'
+require 'rspec-rails'
+DatabaseCleaner.strategy = :truncation
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.include ApiHelper, type: :api
+  config.include Requests::JsonHelpers, type: :api
+
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
