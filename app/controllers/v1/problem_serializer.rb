b83a04956182dcde783fa1371a7e5c5a5b3c429d
wmikefish <wmikefish@gmail.com>
2017-07-18 12:12:05 +0900

    add responded_status to problem api response

--- app/controllers/v1/problem_serializer.rb
+++ app/controllers/v1/problem_serializer.rb
@@ -1,6 +1,6 @@
 module V1
   class ProblemSerializer < ActiveModel::Serializer
     attributes :id, :comment, :image_url, :latitude, :longitude, :user_id,
-               :responses_seen, :created_at, :updated_at
+               :responded, :responses_seen, :created_at, :updated_at
   end
 end
