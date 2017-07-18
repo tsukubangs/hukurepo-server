be6b7971fe629390e7c135d6ba8c31ec89579db0
wmikefish <wmikefish@gmail.com>
2017-07-18 15:04:18 +0900

    fix issue of seen status when post response

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718024937) do
+ActiveRecord::Schema.define(version: 20170718043101) do
 
   create_table "problems", force: :cascade do |t|
     t.text     "comment"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170718024937) do
     t.integer  "user_id",                        null: false
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.boolean  "responses_seen", default: false, null: false
+    t.boolean  "responses_seen", default: true,  null: false
     t.boolean  "responded",      default: false, null: false
   end
 
