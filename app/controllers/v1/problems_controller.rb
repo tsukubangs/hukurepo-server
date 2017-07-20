3e30624778ee56241dd7f633dc61c6780b5ce932
wmikefish <wmikefish@gmail.com>
2017-07-20 17:54:28 +0900

    chage problems response order by desc

--- app/controllers/v1/problems_controller.rb
+++ app/controllers/v1/problems_controller.rb
@@ -4,13 +4,13 @@ module V1
 
     # GET /v1/problems
     def index
-      @problems = Problem.all
+      @problems = Problem.order("updated_at desc")
       render json: @problems, each_serializer: V1::ProblemSerializer
     end
 
     # GET /v1/users/1/problems
     def users
-      @problems = Problem.where(user_id: params[:user_id])
+      @problems = Problem.where(user_id: params[:user_id]).order("updated_at desc")
       render json: @problems, each_serializer: V1::ProblemSerializer
     end
 
