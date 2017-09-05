011dc0963af2c129d1bbe25bd56d45c3f64d510d
vagrant <vagrant@localhost.localdomain>
2017-09-05 05:55:24 +0000

    add escape into response_serializer

--- app/controllers/v1/response_serializer.rb
+++ app/controllers/v1/response_serializer.rb
@@ -1,5 +1,9 @@
 module V1
   class ResponseSerializer < ActiveModel::Serializer
     attributes :id, :comment, :problem_id, :user_id, :created_at, :updated_at
+
+    def comment
+      ERB::Util.html_escape(object.comment)
+    end
   end
 end
