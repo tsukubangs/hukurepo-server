ea50b8ecf54622f208c6c0b5428e86d9887cec47
wmikefish <wmikefish@gmail.com>
2017-09-22 12:08:16 +0900

    add json response 'japanese_comment' to problems api

--- app/controllers/v1/problem_serializer.rb
+++ app/controllers/v1/problem_serializer.rb
@@ -1,12 +1,17 @@
 module V1
   class ProblemSerializer < ActiveModel::Serializer
-    attributes :id, :comment, :image_url, :thumbnail_url, :latitude, :longitude, :user_id,
+    attributes :id, :comment, :japanese_comment, :image_url, :thumbnail_url, :latitude, :longitude, :user_id,
                :responded, :responses_seen, :created_at, :updated_at
     def thumbnail_url
       object.image_url(:thumb)
     end
+
     def comment
       ERB::Util.html_escape(object.comment)
+    end
+
+    def japanese_comment
+      ERB::Util.html_escape(object.japanese_comment)
     end
   end
 end
