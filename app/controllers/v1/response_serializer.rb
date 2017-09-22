38f5594be800bc0321be109a62d3aa0a4c9a4352
wmikefish <wmikefish@gmail.com>
2017-09-22 15:52:00 +0900

    add json response attribute 'japanese_comment' to response'

--- app/controllers/v1/response_serializer.rb
+++ app/controllers/v1/response_serializer.rb
@@ -1,9 +1,13 @@
 module V1
   class ResponseSerializer < ActiveModel::Serializer
-    attributes :id, :comment, :problem_id, :user_id, :created_at, :updated_at
+    attributes :id, :comment, :japanese_comment, :problem_id, :user_id, :created_at, :updated_at
 
     def comment
       ERB::Util.html_escape(object.comment)
+    end
+
+    def japanese_comment
+      ERB::Util.html_escape(object.japanese_comment)
     end
   end
 end
