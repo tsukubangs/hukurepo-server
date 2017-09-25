20848965027d127bda9a22d7e86c0fa00b852f15
wmikefish <wmikefish@gmail.com>
2017-09-25 10:05:31 +0900

    add routes  'problems/me/count'

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,13 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :show] do
       get 'me', on: :collection
       get 'me/problems', to: 'problems#me', on: :collection
+      get 'me/problems/count', to: 'problems#me_count', on: :collection
       put 'me/device_token', to: 'device_tokens#update', on: :collection
       get 'problems', to: 'problems#users'
     end
     resources :problems do
       get 'me', on: :collection
+      get 'me/count', to: 'problems#me_count', on: :collection
       resources :responses, only: [:index, :create] do
         get 'seen', to: 'responses#get_seen', on: :collection
         put 'seen', to: 'responses#put_seen', on: :collection
