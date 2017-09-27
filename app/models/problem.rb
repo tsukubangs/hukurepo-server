353bd5eb1b196c0415ecb0e2f022fd71b3333298
wmikefish <wmikefish@gmail.com>
2017-09-27 14:53:42 +0900

    refactor problem_response_update

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,4 +2,19 @@ class Problem < ApplicationRecord
   mount_uploader :image, ProblemPhotoUploader
   belongs_to :user, foreign_key: "user_id"
   has_many :responses, dependent: :destroy
+
+  # 回答がきたときにproblemの状態（返信済み、回答既読）を更新する
+  # 困りごと投稿ユーザと別人が投稿したときに回答済みフラグrespondedをtrueにする
+  # 困りごとユーザが返信したら再度回答する必要があるため、respondedをfalseにする
+  def update_response_status(response)
+    if response.user != self.user
+      self.responded = true
+      self.responses_seen = false # 回答があったら未読にする
+    else
+      self.responded = false
+      self.responses_seen = true # 自分自身で回答したときは、返信を読んだこととする
+    end
+    self.save
+    self.touch
+  end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,24 +1,13 @@
 class Response < ApplicationRecord
     belongs_to :user, foreign_key: "user_id"
     belongs_to :problem, foreign_key: "problem_id"
-    
+
     def self.new_response(response_params, user, problem)
       ActiveRecord::Base.transaction do
         response = Response.new(response_params)
         response.user = user
 
-        problem.touch
-        # 困りごと投稿ユーザと別人が投稿したときに回答済みフラグrespondedをtrueにする
-        # 困りごとユーザが返信したら再度回答する必要があるため、respondedをfalseにする
-        if response.user != problem.user
-          problem.responded = true
-          problem.responses_seen = false # 回答があったら未読にする
-        else
-          problem.responded = false
-          problem.responses_seen = true # 自分自身で回答したときは、返信を読んだこととする
-        end
-        problem.save!
-
+        problem.update_response_status(response)
         response.problem = problem
 
         response
