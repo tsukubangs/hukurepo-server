2dfef053c57e44e8ef26a41662e9aa9055fb9d2e
wmikefish <wmikefish@gmail.com>
2017-09-27 14:42:39 +0900

    change transaction process when create response

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,26 +1,27 @@
 class Response < ApplicationRecord
     belongs_to :user, foreign_key: "user_id"
     belongs_to :problem, foreign_key: "problem_id"
-
-    # TODO トランザクション処理にする
+    
     def self.new_response(response_params, user, problem)
-      response = Response.new(response_params)
-      response.user = user
+      ActiveRecord::Base.transaction do
+        response = Response.new(response_params)
+        response.user = user
 
-      problem.touch
-      # 困りごと投稿ユーザと別人が投稿したときに回答済みフラグrespondedをtrueにする
-      # 困りごとユーザが返信したら再度回答する必要があるため、respondedをfalseにする
-      if response.user != problem.user
-        problem.responded = true
-        problem.responses_seen = false # 回答があったら未読にする
-      else
-        problem.responded = false
-        problem.responses_seen = true # 自分自身で回答したときは、返信を読んだこととする
+        problem.touch
+        # 困りごと投稿ユーザと別人が投稿したときに回答済みフラグrespondedをtrueにする
+        # 困りごとユーザが返信したら再度回答する必要があるため、respondedをfalseにする
+        if response.user != problem.user
+          problem.responded = true
+          problem.responses_seen = false # 回答があったら未読にする
+        else
+          problem.responded = false
+          problem.responses_seen = true # 自分自身で回答したときは、返信を読んだこととする
+        end
+        problem.save!
+
+        response.problem = problem
+
+        response
       end
-      problem.save!
-
-      response.problem = problem
-
-      response
     end
 end
