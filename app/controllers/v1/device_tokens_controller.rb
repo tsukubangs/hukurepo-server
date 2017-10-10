1348954d03f7a2204c40e4539e0f3412b63a9a69
wmikefish <wmikefish@gmail.com>
2017-10-10 13:34:16 +0900

    allow update user.role via device_token api

--- app/controllers/v1/device_tokens_controller.rb
+++ app/controllers/v1/device_tokens_controller.rb
@@ -1,7 +1,10 @@
 module V1
   class DeviceTokensController < ApplicationController
+    # PUT v1/users/me/device_token
+    # PATCH  v1/users/me/device_token
     def update
-      current_user.device_token = params.permit(:device_token)[:device_token]
+      current_user.role = device_token_params[:role] if device_token_params[:role]
+      current_user.device_token = device_token_params[:device_token]
 
       ActiveRecord::Base.transaction do
         # TODO パスワード無しでの更新
@@ -9,11 +12,17 @@ module V1
         current_user.sweep_same_device_tokens
       end
 
-      render json: { device_token: current_user.device_token }, status: :ok
+      render json: { role: current_user.role, device_token: current_user.device_token }, status: :ok
 
       rescue => e
       render json: { error: e.message }, status: :unprocessable_entity
 
     end
+
+    private
+      def device_token_params
+        # TODO: requireを入れる手段をあとで追加
+        params.permit(:role, :device_token)
+      end
   end
 end
