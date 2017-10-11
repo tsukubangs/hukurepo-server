7b49b2ef67ce721d8db9e9a52b9e7e4c624d1927
wmikefish <wmikefish@gmail.com>
2017-10-11 17:11:05 +0900

    validate user.response_priority

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -6,6 +6,9 @@ class Problem < ApplicationRecord
   scope :responded, -> (value = true) { where(:responded => value) }
   scope :seen, -> (value = true) { where(:seen => value) }
 
+  validates :response_priority, inclusion: { in: %w(high default low),
+message: "allow only 'high' or 'default' or 'low'" }
+
   # 回答がきたときにproblemの状態（返信済み、回答既読）を更新する
   # 困りごと投稿ユーザと別人が投稿したときに回答済みフラグrespondedをtrueにする
   # 困りごとユーザが返信したら再度回答する必要があるため、respondedをfalseにする
