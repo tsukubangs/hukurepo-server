19d9e25820252c44611665731a201a04534461d2
wmikefish <wmikefish@gmail.com>
2017-10-17 13:19:59 +0900

    add seed maneger user

---
+++ db/fixtures/params.rb
@@ -0,0 +1,8 @@
+User.seed do |u|
+  u.email = ENV['MANAGER_EMAIL']
+  u.password = ENV['MANAGER_PASSWORD']
+  u.age = 20
+  u.gender = 'Male'
+  u.country_of_residence = 'Japan'
+  u.role = 'respondent'
+end
