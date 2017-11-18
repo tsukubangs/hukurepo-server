a3088bbdc0c636be46b0f58d0a08e591bf151a57
ShuheiGotou <uhsog.tsukuba@gmail.com>
2017-11-18 18:39:48 +0900

    add test code for google map

--- app/controllers/visualized/problem_maps_controller.rb
+++ app/controllers/visualized/problem_maps_controller.rb
@@ -1,5 +1,10 @@
 class Visualized::ProblemMapsController < ApplicationController
   skip_before_action :authenticate_user_from_token!
   def index
+    problems = Problem.all
+    @hash = Gmaps4rails.build_markers(problems) do |problem, marker|
+      marker.lat problem.latitude
+      marker.lng problem.longitude
+    end
   end
 end
