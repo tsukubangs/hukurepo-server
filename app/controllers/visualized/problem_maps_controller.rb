ae283a14206191385aa1f21ae2798d7d567b7678
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2017-11-20 14:30:34 +0900

    add test info

--- app/controllers/visualized/problem_maps_controller.rb
+++ app/controllers/visualized/problem_maps_controller.rb
@@ -1,10 +1,17 @@
 class Visualized::ProblemMapsController < ApplicationController
   skip_before_action :authenticate_user_from_token!
   def index
-    problems = Problem.all
+    problems = Problem.where.not(latitude: nil, longitude: nil)
     @hash = Gmaps4rails.build_markers(problems) do |problem, marker|
       marker.lat problem.latitude
       marker.lng problem.longitude
+      marker.infowindow render_to_string(partial: "visualized/infowindow", locals: {problem: problem})
+      # marker.picture({
+      #             :url => "http://icon-rainbow.com/i/icon_03702/icon_037020_256.jpg",
+      #             :width   => 32,
+      #             :height  => 32
+      #            })
+      marker.title "TEST"
     end
   end
 end
