26313c4fe1e300afc68f6d4ebc1414cfe57ee3a3
ShuheiGotou <uhsog.tsukuba@gmail.com>
2017-11-20 15:52:12 +0900

    add post_counts to get number of posted data

--- app/controllers/visualized/problem_maps_controller.rb
+++ app/controllers/visualized/problem_maps_controller.rb
@@ -1,6 +1,13 @@
 class Visualized::ProblemMapsController < ApplicationController
   skip_before_action :authenticate_user_from_token!
   def index
+    date = DateTime.now.beginning_of_day
+    @post_counts = {
+        day: Problem.by_day(date).count,
+        month: Problem.by_month(date.month).count,
+        year: Problem.by_year(date.year).count
+    }
+
     problems = Problem.all
     @hash = Gmaps4rails.build_markers(problems) do |problem, marker|
       marker.lat problem.latitude
