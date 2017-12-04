8de49bae4990e2a6f4083064235b3d26a0d15810
ShuheiGotou <uhsog.tsukuba@gmail.com>
2017-12-04 14:50:41 +0900

    join Problem & User to get user info

--- app/controllers/visualized/problems_controller.rb
+++ app/controllers/visualized/problems_controller.rb
@@ -1,6 +1,6 @@
 class Visualized::ProblemsController < ApplicationController
   skip_before_action :authenticate_user_from_token!
   def index
-    @problems = Problem.joins(:user).select("problems.comment, problems.created_at, users.age AS user_age, users.gender AS user_gender")
+    @problems = Problem.joins(:user).select("problems.*, users.age AS user_age, users.gender AS user_gender")
   end
 end
