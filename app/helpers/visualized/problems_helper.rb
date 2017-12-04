18fdbc97cf2e0c35f42db91cf0a9a8cba47e0839
ShuheiGotou <uhsog.tsukuba@gmail.com>
2017-12-04 14:51:43 +0900

    add view for problem card

--- app/helpers/visualized/problems_helper.rb
+++ app/helpers/visualized/problems_helper.rb
@@ -1,2 +1,19 @@
 module Visualized::ProblemsHelper
+
+  def format_date(date)
+    unless date.nil?
+      "#{date.year}/#{date.month}/#{date.day} #{date.hour}:#{date.min}"
+    else
+      "no data"
+    end
+  end
+
+  def add_generation(age)
+    unless age.nil?
+      "#{age}代"
+    else
+      "no data"
+    end
+  end
+
 end
