903f3d9cc098096b969e07f177db438fef4ebb93
ShuheiGotou <uhsog.tsukuba@gmail.com>
2017-12-05 16:46:53 +0900

    fix words in case of nodata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,16 @@ module ApplicationHelper
   def japanese_gender(gender)
     return '男性' if gender == 'male'
     return '女性' if gender == 'female'
-    return 'その他'
+    return '性別不明'
   end
 
   def japanese_date(date)
-    return "不明" if date.nil?
+    return "日時不明" if date.nil?
     "#{date.year}年#{date.month}月#{date.day}日 #{date.hour}時#{date.min}分"
   end
 
   def japanese_generation(age)
-    return "不詳" if age.nil?
+    return "年齢不詳" if age.nil?
     "#{age}代"
   end
 
