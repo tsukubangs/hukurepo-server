aa21ec673c7bbdc71c5130cb1003b8194ef890cf
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2017-12-12 17:42:56 +0900

    fix confirm

--- bin/confirm.rb
+++ bin/confirm.rb
@@ -1,17 +1,34 @@
 require 'net/https'
 require 'uri'
 
-uri = URI.parse("https://" + ENV['DOMAINS'])
+uristr = "https://" + ENV['DOMAINS']
+uri = URI.parse(uristr)
 http = Net::HTTP.new(uri.host, uri.port)
 
 http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
+count = 0
+
 loop do
-  req = Net::HTTP::Get.new(uri.request_uri)
-  res = http.request(req)
-  break if res.code != '404'
+  begin
+    req = Net::HTTP::Get.new(uri.request_uri)
+  rescue
+  end
+    res = http.request(req)
+
+    break if res.code != '404' || count > 99
+    count += 1
+  puts "Connect #{uristr} ...(#{count} times)"
   sleep(3)
 end
 
-puts "Server setup completed!"
+STDOUT.flush
+STDOUT.sync
+
+if count > 99
+  puts "Server Setup Failed"
+else
+  puts "Server Setup Completed"
+end
+
