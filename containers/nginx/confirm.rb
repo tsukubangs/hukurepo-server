005726a13e8a8c0469c3d26ba7d4538fddba0ba6
wmikefish <wmikefish@gmail.com>
2017-12-12 15:48:46 +0900

    add check access rails

---
+++ containers/nginx/confirm.rb
@@ -0,0 +1,17 @@
+require 'net/https'
+require 'uri'
+
+uri = URI.parse("https://" + $ENV['DOMAINS'])
+http = Net::HTTP.new(uri.host, uri.port)
+
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+loop do
+  req = Net::HTTP::Get.new(uri.request_uri)
+  res = http.request(req)
+  sleep(3)
+  break if res.code != '404'
+end
+
+puts "Server setup completed!"
