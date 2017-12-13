2b2ae6af90aa9c6b8a7df359ef31d4a387b58125
wmikefish <wmikefish@gmail.com>
2017-12-13 14:56:18 +0900

    fix manager user

--- db/fixtures/params.rb
+++ db/fixtures/params.rb
@@ -1,6 +1,7 @@
+require 'securerandom'
 User.seed do |u|
   u.id = 1
-  u.email = auto_reply@ngs.com
+  u.email = "auto_reply@ngs.com"
   u.password = SecureRandom.hex(12)
   u.age = 20
   u.gender = 'Male'
