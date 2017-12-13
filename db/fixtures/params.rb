2e6119c81698a4e33492a84a100b135fbcc35af4
wmikefish <wmikefish@gmail.com>
2017-12-13 14:39:41 +0900

    update manager user

--- db/fixtures/params.rb
+++ db/fixtures/params.rb
@@ -1,7 +1,7 @@
 User.seed do |u|
   u.id = 1
-  u.email = ENV['MANAGER_EMAIL']
-  u.password = ENV['MANAGER_PASSWORD']
+  u.email = auto_reply@ngs.com
+  u.password = SecureRandom.hex(12)
   u.age = 20
   u.gender = 'Male'
   u.country_of_residence = 'Japan'
