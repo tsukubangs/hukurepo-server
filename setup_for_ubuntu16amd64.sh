919836eafd054c63d3b55fa04ac3c07a4c48dbd9
Hukuowl <34501660+tsukubangs@users.noreply.github.com>
2017-12-18 16:22:38 +0900

    fix setupshell for ubuntu

--- setup_for_ubuntu16amd64.sh
+++ setup_for_ubuntu16amd64.sh
@@ -1,4 +1,4 @@
-./install-docker-for-ubuntu16amd64.sh & \
-sudo docker-compose build & \
-sudo docker-compose up -d & \
+./install-docker-for-ubuntu16amd64.sh
+sudo docker-compose build
+sudo docker-compose up -d
 sudo docker-compose run --rm web ruby bin/confirm.rb
